a6b6b16f0ec8086e2c96072efd58b099354d6392
JESUS <JESUS@DESKTOP-GIK70J1>
2022-08-17 13:01:47 +0200

    nuestro segundo cambio

--- Repositorio/src/main/java/com/mycompany/repositorio/Repositorio.java
+++ Repositorio/src/main/java/com/mycompany/repositorio/Repositorio.java
@@ -13,6 +13,6 @@ public class Repositorio {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-       Bucles bu =  new Bucles;
+       Bucles bu =  new Bucles();
     }
 }
